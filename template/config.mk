5d1e3bc2f90627fe9776cc3491b684fbf3ca72e1
Geoffroy Van Cutsem <geoffroy.vancutsem@intel.com>
2021-03-18 08:31:47 +0100

    Private Config: only set variables if not already set (#5)
    Update the template for storing one's private configuration items
    to only set the variable if *not* already set. This allows for
    an easily overwriting them from the command-line, e.g.:
    
       UC_MODEL_NAME=my-alt-model make
    
    Signed-off-by: Geoffroy Van Cutsem <geoffroy.vancutsem@intel.com>

--- template/config.mk
+++ template/config.mk
@@ -1,3 +1,3 @@
-SNAP_DEVELOPER_ID   = <Your developer ID>
-SNAP_SIGNING_KEY    = <Your snap signing key name>
-UC_MODEL_NAME       = <Your model assertion name>
+SNAP_DEVELOPER_ID   ?= <Your developer ID>
+SNAP_SIGNING_KEY    ?= <Your snap signing key name>
+UC_MODEL_NAME       ?= <Your model assertion name>
