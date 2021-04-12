d49bbe788d77580e57d2e715fea65a6d9c83a3fa
Tonny Tzeng <tonny.tzeng@gmail.com>
2021-04-12 12:16:14 +0800

    uos: add logic to package ACRN User VM as a snap

---
+++ uos/images/README.md
@@ -0,0 +1,4 @@
+## XZ-compressed OS image for ACRN user VM
+
+Drop in the XZ-compressed OS image in this folder to be packaged as a snap
+for running the user VM on ACRN.
---
+++ uos/launch.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Copyright (C) 2021 Intel Corporation.
+# SPDX-License-Identifier: BSD-3-Clause
+
+PATH=$PATH:/snap/bin
+
+img=uos.img
+
+if [ ! -f $SNAP_COMMON/$img ]; then
+    echo 'Setting up user VM image at' $SNAP_COMMON/$img
+    $SNAP_NAME.xzcat $SNAP/uos/$img.xz > $SNAP_COMMON/$img
+fi
+
+offline_path="/sys/class/vhm/acrn_vhm"
+# Check the device file of /dev/acrn_hsm to determine the offline_path
+if [ -e "/dev/acrn_hsm" ]; then
+offline_path="/sys/class/acrn/acrn_hsm"
+fi
+
+function launch_uos()
+{
+mac=$(cat /sys/class/net/e*/address)
+vm_name=vm$1
+mac_seed=${mac:9:8}-${vm_name}
+
+#check if the vm is running or not
+vm_ps=$(pgrep -a -f acrn.acrn-dm)
+result=$(echo $vm_ps | grep -w "${vm_name}")
+if [[ "$result" != "" ]]; then
+  echo "$vm_name is running, can't create twice!"
+  exit
+fi
+
+#logger_setting, format: logger_name,level; like following
+logger_setting="--logger_setting console,level=4;kmsg,level=3;disk,level=5"
+
+#for pm by vuart setting
+pm_channel="--pm_notify_channel uart "
+pm_by_vuart="--pm_by_vuart pty,/run/acrn/life_mngr_"$vm_name
+pm_vuart_node=" -s 1:0,lpc -l com2,/run/acrn/life_mngr_"$vm_name
+
+#for memsize setting
+mem_size=4096M
+
+acrn.acrn-dm -A -m $mem_size -s 0:0,hostbridge \
+  -s 5,virtio-console,@stdio:stdio_port \
+  -s 6,virtio-hyper_dmabuf \
+  -s 3,virtio-blk,$SNAP_COMMON/$img \
+  -s 4,virtio-net,tap0 \
+  -s 7,virtio-rnd \
+  --ovmf /snap/acrn/current/usr/share/acrn/bios/OVMF.fd \
+  --mac_seed $mac_seed \
+  $vm_name
+}
+
+# offline SOS CPUs except BSP before launch UOS
+for i in `ls -d /sys/devices/system/cpu/cpu[1-99]`; do
+        online=`cat $i/online`
+        idx=`echo $i | tr -cd "[1-99]"`
+        echo cpu$idx online=$online
+        if [ "$online" = "1" ]; then
+            echo 0 > $i/online
+            # during boot time, cpu hotplug may be disabled by pci_device_probe during a pci module insmod
+            while [ "$online" = "1" ]; do
+                sleep 1
+                echo 0 > $i/online
+                online=`cat $i/online`
+            done
+            echo $idx > ${offline_path}/offline_cpu
+        fi
+done
+
+launch_uos 1
